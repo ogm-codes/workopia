0b91d6b0351b392c35e10caf10e31076b3f1b395
IT Learner 13 <itlnr13@nwk.co.za>
2024-09-23 09:59:45 +0200

    UI broken into partials

--- helpers.php
+++ helpers.php
@@ -9,3 +9,13 @@
 function base_path($path = '') {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function loadView($name) {
+    require base_path("views/{$name}.view.php");
+}
